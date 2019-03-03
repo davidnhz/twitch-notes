a48bb690551d05a4d2cd6d4783d855adeebfaa6e
davidnhz <davidnhz@gmail.com>
2019-03-03 16:51:51 -0600

    Add twitch conf to services

--- config/services.php
+++ config/services.php
@@ -44,4 +44,10 @@ return [
         ],
     ],
 
+    'twitch' => [
+        'client_id' => env('TWITCH_KEY'),
+        'client_secret' => env('TWITCH_SECRET'),
+        'redirect' => env('TWITCH_REDIRECT_URI'),
+    ],
+
 ];
