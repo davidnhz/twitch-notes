194ab4cde828ad2578dbfea2063a1faa8537c70f
davidnhz <davidnhz@gmail.com>
2019-03-03 16:54:00 -0600

    Implement social auth

---
+++ app/Http/Controllers/SocialAuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Socialite;
+use SocialiteProviders\Manager\ServiceProvider;
+use App\User;
+use Auth;
+
+class SocialAuthController extends Controller
+{
+    public function callback(){
+        $user = $this->createOrGetUser(Socialite::driver('twitch'));
+        auth()->login($user);
+
+        return redirect()->to('/');
+    }
+
+    public function redirect() {
+        return Socialite::driver('twitch')->redirect();
+    }
+
+    private function createOrGetUser(\SocialiteProviders\Twitch\Provider $provider) {
+        $auth_user = $provider->user();
+
+        $user = User::whereTwitchId($auth_user->getId())
+            ->first();
+
+        if(!$user) {
+            $user = User::create([
+                'email' => $auth_user->getEmail(),
+                'username' => $auth_user->getNickname(),
+                'twitch_id' => $auth_user->getId(),
+                'avatar' => $auth_user->getAvatar(),
+            ]);
+        }
+
+        return $user;
+    }
+
+    public function logout(Request $request) {
+        Auth::logout();
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('auth/callback/twitch', 'SocialAuthController@callback');
+Route::get('auth/redirect/twitch', 'SocialAuthController@redirect');
