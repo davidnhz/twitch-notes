c5963ca8235021b0aee5001e32e59b8a545ba8cc
davidnhz <davidnhz@gmail.com>
2019-03-03 16:20:36 -0600

    Update users migration social fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('twitch_id')->unsigned();
             $table->string('username');
+            $table->string('avatar');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
