36dbd5ed72cbbe6f120b4138b5f9e80b921b8d4c
davidnhz <davidnhz@gmail.com>
2019-03-03 16:52:31 -0600

    Fix user migration for socialite

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,11 +17,11 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->integer('twitch_id')->unsigned();
             $table->string('username');
-            $table->string('avatar');
+            $table->string('avatar')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('token');
+            $table->string('password')->nullable();
+            $table->string('token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
