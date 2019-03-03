8abb1797d1a49a7c7fb66535e62169726192622f
davidnhz <davidnhz@gmail.com>
2019-03-03 16:16:55 -0600

    Update users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('token');
             $table->rememberToken();
             $table->timestamps();
         });
