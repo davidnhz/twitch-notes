6752ca34c4f2d666aa24611f5e2666dea160edf6
davidnhz <davidnhz@gmail.com>
2019-03-13 10:57:01 -0600

    Improve welcome page text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
     <div class="content">
         <h4>Welcome to Twitch Notes!</h4>
         <p>Twitcher Notes is an application where you can create personal notes during live streams of your favorite streamers.</p>
-        <p>You just have to add the nickname of your favorites streamers and then you are going to be able to add notes while you are watching the live stream.</p>
-        <p>You can revisit your ideas and comments created during streams for education, analysis or entertainment purposes.</p>
+        <p>Add the nickname of your favorites streamers and then create notes while you are watching the live stream.</p>
+        <p>You can revisit your ideas and comments created during streams for analysis or just for fun.</p>
         @auth
             <p>Go to <a href="{{ url('/streamers') }}">Streamers</a> to start!</p>
         @else
